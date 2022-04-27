8740ff557c66c1d8963e938bafe0d8ca618bb6fc
ol.ilyassov <ol.ilyassov@gmail.com>
2022-04-27 12:00:55 +0600

    Add announcement

--- src/app/add-announcement/add-announcement.page.ts
+++ src/app/add-announcement/add-announcement.page.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAnnouncementPage implements OnInit {
 
+  title: string;
+  details: string;
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
